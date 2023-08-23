6d27a8e2fbe5420580cd87e4d63f00e382a86fec
manyu <manyu14630@gmail.com>
2023-08-24 01:05:01 +0800

    Update firstAndLastPosition.java
    add some comments

--- firstAndLastPosition.java
+++ firstAndLastPosition.java
@@ -6,7 +6,7 @@ public class firstAndLastPosition {
 
         int left = 0, right = nums.length - 1;
 
-        while(left < right){
+        while(left < right){//还是不懂为什么这样分大于等于target
             int mid = left + (right - left) / 2;
             if(nums[mid] >= target){
                 right = mid;
@@ -18,7 +18,8 @@ public class firstAndLastPosition {
             return new int[] {-1, -1};
         }
 
-        int Left = right;
+        int Left = right;//从第一个binary search出来的条件就说left = right 所以这里等于哪个都一样
+        //这个命名有歧义 应该用firstPoint
         left = 0; right = nums.length - 1;
 
         while(left < right){
