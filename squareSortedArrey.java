bace75efa1df718c92df031e4a1fed274a2b788e
manyu <manyu14630@gmail.com>
2023-08-24 23:20:00 +0800

    977 square the sorted array
    a sorted array, square it and resort it

---
+++ squareSortedArrey.java
@@ -0,0 +1,20 @@
+public class squareSortedArrey {
+    public int[] sortedSquares(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int[] result = new int[nums.length];
+        int index = result.length - 1;
+
+        while(left <= right){
+            if(nums[left] * nums[left] > nums[right] * nums[right]){
+                result[index] = nums[left] * nums[left];
+                index--;
+                left++;
+            } else{
+                result[index] = nums[right] * nums[right];
+                index--;
+                right--;
+            }
+        }
+        return result;
+}
