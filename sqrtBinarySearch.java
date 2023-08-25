c14fbbc7f75736dbee4e6dfacdfd01c1a623adc0
manyu <manyu14630@gmail.com>
2023-08-25 16:32:58 +0800

    rename the int

--- sqrtBinarySearch.java
+++ sqrtBinarySearch.java
@@ -1,13 +1,13 @@
 public class sqrtBinarySearch {
     public int mySqrt(int x) {
-        int l = 0, r = x, ans = -1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
+        int left = 0, right = x, ans = 0;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
             if ((long) mid * mid <= x) {
                 ans = mid;
-                l = mid + 1;
+                left = mid + 1;
             } else {
-                r = mid - 1;
+                right = mid - 1;
             }
         }
         return ans;
