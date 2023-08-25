d48f28256c0d067109f68d324e09e18449c00bb7
manyu <manyu14630@gmail.com>
2023-08-25 16:21:39 +0800

    209 reverse the linked list
    lc 209, reverse the linked list

---
+++ reverseLinkedList.java
@@ -0,0 +1,14 @@
+public class reverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode cur = head;
+        ListNode temp = null;
+        while (cur != null) {
+            temp = cur.next;// 保存下一个节点
+            cur.next = prev;
+            prev = cur;
+            cur = temp;
+        }
+        return prev;
+    }
+}
