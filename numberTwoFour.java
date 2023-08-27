59c968b1fbd47913c212d09b1ed835f07146be8e
manyu <manyu14630@gmail.com>
2023-08-28 01:31:24 +0800

    lc 24
    exchange

---
+++ numberTwoFour.java
@@ -0,0 +1,15 @@
+public class numberTwoFour {
+    public ListNode swapPairs(ListNode head) {
+        // base case 退出提交
+        if(head == null || head.next == null) return head;
+        // 获取当前节点的下一个节点
+        ListNode next = head.next;
+        // 进行递归
+        ListNode newNode = swapPairs(next.next);
+        // 这里进行交换
+        next.next = head;
+        head.next = newNode;
+
+        return next;
+    }
+}
