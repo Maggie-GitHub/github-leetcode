f917b2387e36717d44e14d6ff097786eb8b494de
manyu <manyu14630@gmail.com>
2023-08-29 22:01:26 +0800

    using hash table
    using hash table to solve the problem, lc question 242

---
+++ validAnagram.java
@@ -0,0 +1,18 @@
+public class validAnagram {
+    public boolean isAnagram(String s, String t) {
+        int[] hash = new int[26];
+
+        for(int i = 0; i < s.length(); i++){
+            hash[s.charAt(i) - 'a']++;
+        }
+        for(int i = 0; i < t.length(); i++){
+            hash[t.charAt(i) - 'a']--;
+        }
+        for(int count : hash){
+            if(count != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
