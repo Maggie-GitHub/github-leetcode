638553afa944ccf524ae3b4eec17828bf04b8dfc
manyu <manyu14630@gmail.com>
2023-08-31 22:45:10 +0800

    RansonNote baoli

--- RansomNoteBaoli.java
+++ RansomNoteBaoli.java
@@ -2,14 +2,15 @@ public class RansomNoteBaoli {
     public boolean canConstruct(String ransomNote, String magazine) {
         for (int i = 0; i < magazine.length(); i++) {
             for (int j = 0; j < ransomNote.length(); j++){
-                if(magazine.charAt(i) == ransomNote.charAt(j)){
+                if(magazine.charAt(i) == ransomNote.charAt(j)){//change int to string
                     ransomNote = ransomNote.substring(0, j) + ransomNote.substring(j + 1);
+                    //(0, j)范围是(0, j-1), 后面+ (j+1), 由此把j删掉
                     break;
                 }
             }
         }
         if(ransomNote.length() == 0){
-            return true;
+            return true;//if there is no element in the ransomNote
         }
         return false;
     }
