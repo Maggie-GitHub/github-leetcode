c2c30f1a05088cc1c324a2e86b11ecf8f65c8f78
manyu <manyu14630@gmail.com>
2023-09-05 01:43:38 +0800

    Update ImplementStack.java
    add some comments to show how to solve the problem

--- ImplementStack.java
+++ ImplementStack.java
@@ -1,5 +1,10 @@
 import java.util.ArrayDeque;
 import java.util.Queue;
+//this method is using two queues to finish the task
+//first queue send size个元素 to queue2
+//then add the x to queue1
+//then queue2 give back size个元素 to que1
+//finally, the one comes out is what we want - x
 
 public class ImplementStack {
     Queue<Integer> que1 = new ArrayDeque<>();
@@ -10,17 +15,17 @@ public class ImplementStack {
     }
 
     public void push(int x) {
-        while(que1.size() > 0){
+        while(que1.size() > 0){//size个元素 to queue2
             que2.add(que1.poll());
         }
-        que1.add(x);
+        que1.add(x);//add the x to queue1
         while(que2.size() > 0){
-            que1.add(que2.poll());
+            que1.add(que2.poll());//size个元素 to queue1
         }
     }
 
     public int pop() {
-        return que1.poll();
+        return que1.poll();//same as pop for stack
     }
 
     public int top() {
