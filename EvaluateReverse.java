697d52a9ba6901ecf620c43034c13638eb475af9
manyu <manyu14630@gmail.com>
2023-09-05 19:53:56 +0800

    add some comments

--- EvaluateReverse.java
+++ EvaluateReverse.java
@@ -5,20 +5,20 @@ public class EvaluateReverse {
     public int evalRPN(String[] tokens) {
         Deque<Integer> stack = new LinkedList();
         for(String s : tokens){
-            if("+".equals(s)){
+            if("+".equals(s)){//+
                 stack.push(stack.pop() + stack.pop());
-            } else if("-".equals(s)){
+            } else if("-".equals(s)){//-
                 stack.push(-stack.pop() + stack.pop());
-            } else if("*".equals(s)){
+            } else if("*".equals(s)){//*
                 stack.push(stack.pop() * stack.pop());
-            } else if("/".equals(s)){
+            } else if("/".equals(s)){//除
                 int temp1 = stack.pop();
                 int temp2 = stack.pop();
                 stack.push(temp2 / temp1);
-            } else{
+            } else{//最终的结果
                 stack.push(Integer.valueOf(s));
             }
         }
-        return stack.pop();
+        return stack.pop();//pop the final result
     }
 }
