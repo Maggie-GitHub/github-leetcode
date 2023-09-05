aa2deab139791a7a417a4dda9e2169b01bf12b07
manyu <manyu14630@gmail.com>
2023-09-05 14:03:37 +0800

    add some comments

--- ValidParentheses.java
+++ ValidParentheses.java
@@ -1,8 +1,11 @@
 import java.util.Stack;
-
+//there are only three cases in this question
+//1: ({[]}((( list is empty, stack still left some elements, so false
+//2: ({)}  stack's peek won't match the chatAt, so false
+//3: ({[]}))) element left, but stack empty, so false
 public class ValidParentheses {
     public boolean isValid(String s) {
-        if(s.length() % 2 != 0){
+        if(s.length() % 2 != 0){//if it has odd numbers of element, it must be false
             return false;
         }
         Stack<Character> stack = new Stack<>();
